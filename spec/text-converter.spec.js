9082b7054de415e290585f000e1ab0633ae4a4c8
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-08-19 11:19:05 +0100

    Added more tests, moved date checking to own function. Fixes #1, fixes #2.

--- spec/text-converter.spec.js
+++ spec/text-converter.spec.js
@@ -181,3 +181,127 @@ describe('parsing "WLD - Iran migration.txt"', () => {
     it('should be able to clean data', () => assertData);
   });
 });
+
+describe('parsing "Datawatch_Monday.txt"', () => {
+  const path = resolve(__dirname, 'mocks', 'Datawatch_Monday.txt');
+
+  function assertComments() {
+    comments.should.have.lengthOf(8);
+    comments[0].should.eql(['Men', 'Women']);
+    comments[1].should.eql(['title', 'Women consistently vote for the Democratic candidate']);
+    comments[2].should.eql(['subtitle', '% who voted for the Democratic candidate according to US exit polls']);
+    comments[3].should.eql(['source', 'Pew Research Centre']);
+    comments[4].should.eql(['footnote', 'delete if not required']);
+    comments[5].should.eql(['comment', 'Line chart']);
+    comments[6].should.eql(['doublescale', '0']);
+    comments[7].should.eql(['accumulate', 'false']);
+  }
+
+  function assertData() {
+    output.should.have.lengthOf(12);
+    output[0].should.eql(['date', 'Men', 'Women']);
+    output[1].should.eql([1972, 36, 38]);
+    output[2].should.eql([1976, 50, 50]);
+    output[3].should.eql([1980, 35, 45]);
+    output[4].should.eql([1984, 37, 44]);
+    output[5].should.eql([1988, 41, 49]);
+    output[6].should.eql([1992, 41, 45]);
+    output[7].should.eql([1996, 43, 54]);
+    output[8].should.eql([2000, 42, 54]);
+    output[9].should.eql([2004, 44, 51]);
+    output[10].should.eql([2008, 49, 56]);
+    output[11].should.eql([2012, 45, 55]);
+  }
+
+  describe('loading data as Buffer', () => {
+    const data = readFileSync(path);
+    const comments = extractComments(data);
+    const output = cleanInput(data, comments);
+
+    it('should be able to parse comments', () => assertComments);
+    it('should be able to clean data', () => assertData);
+  });
+
+  describe('loading data as String', () => {
+    const data = readFileSync(path, {encoding: 'utf8'});
+    const comments = extractComments(data);
+    const output = cleanInput(data, comments);
+
+    it('should be able to parse comments', () => assertComments);
+    it('should be able to clean data', () => assertData);
+  });
+});
+
+describe('parsing "UKRetail1.txt"', () => {
+  const path = resolve(__dirname, 'mocks', 'UKRetail1.txt');
+
+  function assertComments() {
+    comments.should.have.lengthOf(7);
+    comments[0].should.eql(['3-m']);
+    comments[1].should.eql(['title', 'UK retail sales volume']);
+    comments[2].should.eql(['subtitle', '% change, latest three months on same period a year earlier']);
+    comments[3].should.eql(['source', 'Thomson Reuters Datastream']);
+    comments[4].should.eql(['comment', 'Line chart']);
+    comments[5].should.eql(['doublescale', '0']);
+    comments[6].should.eql(['accumulate', 'false']);
+  }
+
+  function assertData() {
+    output.should.have.lengthOf(67);
+    output[0].should.eql(['date', '3-m']);
+    output[1].should.eql(['15 Jan 2011', 0.068027211]);
+    output[66].should.eql(['15 Jul 2016', 5.279407224]);
+  }
+
+  describe('loading data as Buffer', () => {
+    const data = readFileSync(path);
+    const comments = extractComments(data);
+    const output = cleanInput(data, comments);
+
+    it('should be able to parse comments', () => assertComments);
+    it('should be able to clean data', () => assertData);
+  });
+
+  describe('loading data as String', () => {
+    const data = readFileSync(path, {encoding: 'ucs2'});
+    const comments = extractComments(data);
+    const output = cleanInput(data, comments);
+
+    it('should be able to parse comments', () => assertComments);
+    it('should be able to clean data', () => assertData);
+  });
+});
+
+
+// New dataset template
+// describe('parsing ".txt"', () => {
+//   const path = resolve(__dirname, 'mocks', '.txt');
+//
+//   function assertComments() {
+//     comments.should.have.lengthOf(8);
+//     comments[0].should.eql([]);
+//   }
+//
+//   function assertData() {
+//     output.should.have.lengthOf();
+//     output[0].should.eql([]);
+//   }
+//
+//   describe('loading data as Buffer', () => {
+//     const data = readFileSync(path);
+//     const comments = extractComments(data);
+//     const output = cleanInput(data, comments);
+//
+//     it('should be able to parse comments', () => assertComments);
+//     it('should be able to clean data', () => assertData);
+//   });
+//
+//   describe('loading data as String', () => {
+//     const data = readFileSync(path, {encoding: 'ucs2'});
+//     const comments = extractComments(data);
+//     const output = cleanInput(data, comments);
+//
+//     it('should be able to parse comments', () => assertComments);
+//     it('should be able to clean data', () => assertData);
+//   });
+// });
